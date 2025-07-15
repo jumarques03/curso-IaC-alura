60be07bba30c1f9a37217394097f0b597e3b8e48
jumarques03 <jumarques631@gmail.com>
2025-07-15 15:05:37 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,9 +5,18 @@ provider "aws" {
 resource "aws_instance" "curso-IaC" {
   ami           = "ami-0150ccaf51ab55a51"
   instance_type = "t2.micro"
-  key_name = "curso-IaC"
+  key_name      = "curso-IaC"
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y python3
+              cd /home/ec2-user
+              echo "<h1>Feito com Terraform</h1>" > index.html
+              nohup python3 -m http.server 8080 &
+            EOF
+
 
   tags = {
-    Name = "Instancia do Curso de IaC"
+    Name = "curso-IaC"
   }
 }
