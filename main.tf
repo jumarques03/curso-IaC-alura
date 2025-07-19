3b6f69c28e02c46c988ecb002475bba656c72cd7
Júlia Marques <jumarques631@gmail.com>
2025-07-19 16:16:32 -0300

    Updated main.tf

--- main.tf
+++ main.tf
@@ -2,21 +2,79 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "curso-IaC" {
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "VPC-Terraform"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.0.0/28"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public_Subnet-Terraform"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  
+  tags = {
+    Name = "Internet_Gateway-Terraform"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "public_route" {
+  route_table_id = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_route_assoc" {
+  subnet_id = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_security_group" "sg_SSH" {
+  name = "SSH_Acess"
+  description = "Allows SSH acess"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Security_Group-Terraform"
+  }
+}
+
+resource "aws_instance" "vm" {
   ami           = "ami-0150ccaf51ab55a51"
   instance_type = "t2.micro"
   key_name      = "curso-IaC"
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install -y python3
-              cd /home/ec2-user
-              echo "<h1>Feito com Terraform</h1>" > index.html
-              nohup python3 -m http.server 8080 &
-            EOF
-
-
-  tags = {
-    Name = "curso-IaC"
-  }
+  subnet_id = aws_subnet.public_subnet.id
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.sg_SSH.id]
+  
 }
